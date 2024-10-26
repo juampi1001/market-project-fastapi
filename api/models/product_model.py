bc83a31edfccc48295736676587af122f320a82f
juampi1001 <vitalevijuampi105@gmail.com>
2024-10-26 00:56:56 -0300

    Category Enum added to products model

--- api/models/product_model.py
+++ api/models/product_model.py
@@ -1,14 +1,31 @@
-from typing import Optional
-from pydantic import BaseModel
+from typing import List, Optional
+from pydantic import BaseModel, Field
 from pydantic.json_schema import SkipJsonSchema
+from enum import Enum
+
+class Category(str, Enum):
+    ELECTRONICS = "electronics"
+    FASHION = "fashion"
+    HOME_FURNITURE = "home_furniture"
+    BEAUTY_HEALTH = "beauty_health"
+    SPORTS_OUTDOORS = "sports_outdoors"
+    TOYS_HOBBIES = "toys_hobbies"
+    FOOD_GROCERIES = "food_groceries"
+    AUTOMOTIVE = "automotive"
+    BABY_KIDS = "baby_kids"
+    OFFICE_SUPPLIES = "office_supplies"
+    OTHERS = "others"
 
 class Product(BaseModel):
     id: SkipJsonSchema[str] = None
     name: str
+    images: List[str]
     description: str
     price: float
     final_price: SkipJsonSchema[float] = None
     discount: Optional[float]
     quantity: int
+    category: Category
+    tags: Optional[List[str]] = None 
     creation_datetime: SkipJsonSchema[str] = None
     modification_datetime: SkipJsonSchema[str] = None
